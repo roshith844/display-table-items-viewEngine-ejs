eb072d906a7485aa75ea38b1518865da8630dc1a
Roshith P <roshith844@gmail.com>
2022-10-24 13:57:42 +0530

    Add : Table

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
+const info = [
+     {
+          name: 'roshith',
+          age : 23,
+          domain: 'MEAN'
+     },
+     {
+          name: 'vahid',
+          age : 22,
+          domain: 'MERN'
+     }
+]
+
 app.set('view engine','ejs')
 app.get('/', (req,res)=>{
-     res.render('home')
+     res.render('home',{info})
 })
+
 
 // server
 http.createServer(app).listen(9000,()=>{
